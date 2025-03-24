c9938b43ba63a3eda47e24d9b41740e2df10d994
Boris <borisngha6@gmail.com>
2025-03-24 08:46:31 +0100

    update: add function to build discord message

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,18 @@ use crate::discord::send_discord_message;
 use actix_web::{App, HttpResponse, HttpServer, Responder, web};
 
 async fn handle_webhook(payload: web::Json<AlertmanagerPayload>) -> impl Responder {
+    let message = build_discord_message(&payload);
+    let discord_webhook_url = get_discord_webhook_url();
+    match send_discord_message(&discord_webhook_url, &message).await {
+        Ok(_) => HttpResponse::Ok().body("OK"),
+        Err(e) => {
+            eprintln!("Error sending to Discord: {}", e);
+            HttpResponse::InternalServerError().body("Error sending to Discord")
+        }
+    }
+}
+
+fn build_discord_message(payload: &AlertmanagerPayload) -> String {
     let mut message = String::from("Alerts:\n");
     for alert in &payload.alerts {
         let alert_name = &alert.labels.alertname;
@@ -23,15 +35,7 @@ async fn handle_webhook(payload: web::Json<AlertmanagerPayload>) -> impl Respond
             alert_name, status, summary, description
         ));
     }
-
-    let discord_webhook_url = get_discord_webhook_url();
-    match send_discord_message(&discord_webhook_url, &message).await {
-        Ok(_) => HttpResponse::Ok().body("OK"),
-        Err(e) => {
-            eprintln!("Error sending to Discord: {}", e);
-            HttpResponse::InternalServerError().body("Error sending to Discord")
-        }
-    }
+    message
 }
 
 #[actix_web::main]
